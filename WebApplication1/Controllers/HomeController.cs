f3edae36323ef5521f36336d795e3695619cbdc0
VenkateshwaranSaravanakumar <venkateshwaran.s@syncfusion.com>
2023-08-29 21:50:02 +0530

    Added files for copy paste functionality in Scheduler

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.appointments = GetScheduleData();
+            return View();
+        }
+
+        public List<AppointmentData> GetScheduleData()
+        {
+            List<AppointmentData> appData = new List<AppointmentData>();
+            appData.Add(new AppointmentData
+            {
+                Subject = "Paris",
+                StartTime = new DateTime(2018, 2, 15, 10, 0, 0),
+                EndTime = new DateTime(2018, 2, 15, 12, 30, 0)
+            });
+            return appData;
+        }
+        public class AppointmentData
+        {
+            public string Subject { get; set; }
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+        }
+
+
+
+    }
+}
